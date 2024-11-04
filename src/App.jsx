a33ecc4b14f2dd0e5a6b2fcd9acae92c24ce7cc3
Karen Colon <kscaraballo@hotmail.com>
2024-11-04 12:00:23 -0500

    cambios

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { ApiBasic } from './components/ApiBasic';
+import { CatGalleryFetch } from './components/CatGalleryFetch';
+
+function App() {
+
+
+  return (
+    <div>
+     <CatGalleryFetch />
+    </div>
+  )
+}
+
+export default App
